3f45962cc45df84f7483648e75200bd1590d0837
Andreas <113241034+andreaszac@users.noreply.github.com>
2025-04-06 10:27:12 +0300

    Time: 437 ms (95.94%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,12 +1,10 @@
 /* Write your PL/SQL query statement below */
 WITH previous_day_temp AS(
-SELECT id,temperature, recordDate, LAG(temperature) OVER(ORDER BY recordDate) previous_day_temperature
+SELECT id,temperature, recordDate, 
+LAG(temperature) OVER(ORDER BY recordDate) previous_day_temperature,
+LAG(recordDate) OVER (ORDER BY recordDate) previous_day
 FROM Weather
-),
-previous_date AS(
-    SELECT id,temperature,recordDate, previous_day_temperature, LAG(recordDate) OVER (ORDER BY recordDate) previous_day
-    FROM previous_day_temp
 )
 SELECT id
-FROM previous_date
+FROM previous_day_temp
 WHERE (temperature - previous_day_temperature) > 0 AND recordDate = previous_day + 1
